2f8e56af8b2c5a3192976e4a05394508dce470f0
_ñ <knaka0209@gmail.com>
2024-02-18 14:56:02 +0900

    up, 0218a14

--- src/client/About.svelte
+++ src/client/About.svelte
@@ -5,7 +5,7 @@ import Head from '../components/Head.svelte';
 
 <!-- -->
 <main>
-  <div>
+  <div class="container mx-auto my-2 px-8 bg-white">
     <Head />
     <hr />
     <h1 class="text-4xl font-bold">About</h1>
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -14,12 +14,14 @@ export default function Page() {
             )} 
         </head>
         <body>
+          <div className="container mx-auto my-2 px-8 bg-white">
             <a href="/">[ home ]</a>
             <a href="/about">[ about ]</a>
             <a href="/test">[ test ]</a>
             <hr />
             <h1 className="text-4xl font-bold">Hello!</h1>
             <div id="app"></div>
+          </div>
         </body>
     </html>
     );
