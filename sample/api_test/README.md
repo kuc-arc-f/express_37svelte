5c91d25caf51afcd170e2fc5ecf78f5316b0fc01
_ñ <knaka0209@gmail.com>
2024-03-05 08:20:40 +0900

    up, 0305a7

---
+++ sample/api_test/README.md
@@ -0,0 +1,31 @@
+﻿# api_test
+
+ Version: 0.9.1
+
+ Author  : Kouji Nakashima / kuc-arc-f.com
+
+ date    : 2024/02/20
+
+ update :
+
+***
+
+api connect  sample
+
+***
+### API : workers + D1
+
+https://github.com/kuc-arc-f/hono_34api
+
+
+***
+### .env
+
+* EXTERNAL_API_URL set
+
+```
+EXTERNAL_API_URL="http://localhost"
+```
+****
+
+
---
+++ sample/api_test/src/components/Head.svelte
@@ -0,0 +1,20 @@
+<script>
+//import {link} from 'svelte-spa-router'
+</script>
+
+<header class="">
+  <div class="container">
+    <span><a href="/">[ Home ]</a>
+    </span>
+    <span><a href="/about">[ about ]</a>
+    </span>
+    <span><a href="/test">[ test ]</a>
+    </span>
+    <span><a href="/test_api">[ testApi ]</a>
+    </span>
+  </div>
+  <hr />
+</header>
+
+<!--
+-->
