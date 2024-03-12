c5bfbcd1e483cfc3c474f44b6e3b50a8d278dc0e
_ñ <knaka0209@gmail.com>
2024-03-12 20:54:51 +0900

    up, 0312a12

---
+++ sample/todo/src/components/Head.svelte
@@ -0,0 +1,22 @@
+<script>
+//import {link} from 'svelte-spa-router'
+</script>
+
+<header class="">
+  <div class="container">
+    <span><a href="/">[ Home ]</a>
+    </span>
+    <span><a href="/about">[ about ]</a>
+    </span>
+    <span><a href="/test">[ test ]</a>
+    </span>
+    <span><a href="/todos">[ Todo ]</a>
+    </span>
+  </div>
+  <hr />
+</header>
+
+<!--
+<span><a href="/test_api">[ testApi ]</a>
+</span>
+-->
